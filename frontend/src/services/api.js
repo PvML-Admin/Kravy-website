cac87685c7a399c8b0aab87e26bf3e18170123fe
Alexandre <afkbro.alex@gmail.com>
2025-10-18 22:57:38 +0100

    what u doing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL = 'https://kravy-backend.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL = 'https://kravy-website.onrender.com/api'
 });
 
 export const membersAPI = {
