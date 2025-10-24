56e740a6aaec35dcd69e737afd14d93724a62ebe
Alexandre <afkbro.alex@gmail.com>
2025-10-24 18:27:21 +0100

    icon fix

--- backend/src/utils/activityCategorizer.js
+++ backend/src/utils/activityCategorizer.js
@@ -1,8 +1,8 @@
 const SKILLS = [
   'Attack', 'Defence', 'Strength', 'Constitution', 'Ranged', 'Prayer',
   'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking',
-  'Crafting', 'Smithing', 'Mining', 'Herlore', 'Agility', 'Thieving',
-  'Slayer', 'Farming', 'Runecrafting', 'Hunter', 'Construction',
+  'Runecrafting', 'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility', 'Thieving',
+  'Slayer', 'Farming', 'Hunter', 'Construction',
   'Summoning', 'Dungeoneering', 'Divination', 'Invention', 'Archaeology', 'Necromancy'
 ];
 
--- frontend/src/utils/skills.js
+++ frontend/src/utils/skills.js
@@ -41,7 +41,7 @@ export const getSkillIcon = (skillName) => {
 export const skillOrder = [
   'Overall', 'Attack', 'Defence', 'Strength', 'Constitution', 'Ranged', 'Prayer',
   'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking',
-  'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility', 'Thieving',
-  'Slayer', 'Farming', 'Runecrafting', 'Hunter', 'Construction',
+  'Runecrafting', 'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility', 'Thieving',
+  'Slayer', 'Farming', 'Hunter', 'Construction',
   'Summoning', 'Dungeoneering', 'Divination', 'Invention', 'Archaeology', 'Necromancy'
 ];
